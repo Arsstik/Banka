e41fcfed1a29f42f9c4646d73faba483a85280d3
GachiBusik <Vadim.tselishev@mail.ru>
2022-05-17 20:11:55 +0300

    -com

--- Test/Test/MainWindow.xaml.cs
+++ Test/Test/MainWindow.xaml.cs
@@ -15,9 +15,6 @@ using System.Windows.Shapes;
 
 namespace Test
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
